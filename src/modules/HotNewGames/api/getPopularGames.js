a8f780394f40faa382728490a70218fb7c3cf8c5
tragedy51 <feqt26@gmail.com>
2025-02-10 21:31:16 +0500

    feat: filter parameter in rent page changed

--- src/modules/HotNewGames/api/getPopularGames.js
+++ src/modules/HotNewGames/api/getPopularGames.js
@@ -2,7 +2,7 @@ export async function getPopularGames(dateFilter) {
 	const response = await fetch(
 		`${
 			import.meta.env.VITE_API_URL
-		}/catalog/top-popular?release_date_period=${dateFilter}&limit=30`
+		}/catalog/top-popular?period=${dateFilter}&limit=10`
 	);
 	const result = await response.json();
 
